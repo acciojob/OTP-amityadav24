5c1df49c7f024788da7f8e40f677f5295b0943ed
Amit Yadav <103848489+amityadav24@users.noreply.github.com>
2025-06-19 07:53:52 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,31 @@
-//your JS code here. If required.
+const inputs = document.querySelectorAll('.code');
+
+window.addEventListener('DOMContentLoaded', () => {
+  inputs[0].focus();
+});
+
+inputs.forEach((input, index) => {
+  input.addEventListener('input', (e) => {
+    const value = e.target.value;
+
+    if (value) {
+      if (index < inputs.length - 1) {
+        inputs[index + 1].focus();
+      }
+    }
+  });
+
+  input.addEventListener('keydown', (e) => {
+    const key = e.key;
+
+    if (key === 'Backspace') {
+      if (input.value === '') {
+        if (index > 0) {
+          inputs[index - 1].focus();
+        }
+      } else {
+        input.value = '';
+      }
+    }
+  });
+});
